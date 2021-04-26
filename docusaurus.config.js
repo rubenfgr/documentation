90e22ff676a6007397e39b6139492f1cade284e5
rubenfgr <rubenfgr87@outlook.com>
2021-04-26 17:53:34 +0200

    Git commands atached

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,12 +3,13 @@ module.exports = {
   title: 'RubenFGR Documentación',
   tagline: 'Documentación para el desarrollo de aplicaciones multiplataforma y web, sistemas e iot',
   url: 'https://blogdeldesarrollador.com',
-  baseUrl: '/',
+  baseUrl: '/documentation/',
+  // baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'https://raw.githubusercontent.com/rubenfgr/rubenfgr.github.io/b5205014cf5535234f31c5656431a493667a8121/assets/img/logotipo.svg',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rubenfgr-doc', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'Ruben FGR - Documentación',
